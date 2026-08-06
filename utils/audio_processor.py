297bf4a8786734d2b7fbc3ac99ed3b113b48bbe2
venom312004 <dheerajpandey312004@gmail.com>
2026-08-06 12:48:21 +0530

    Update fresh YouTube cookies and add extractor args

--- utils/audio_processor.py
+++ utils/audio_processor.py
@@ -2,15 +2,23 @@ import yt_dlp
 from pydub import AudioSegment
 import os
 
-DOWNLOAD_DIR="downloads"
-os.makedirs(DOWNLOAD_DIR,exist_ok=True)
+DOWNLOAD_DIR = "downloads"
+os.makedirs(DOWNLOAD_DIR, exist_ok=True)
 
-def download_youtube_audio(url:str)->str:
+def download_youtube_audio(url: str) -> str:
     output_path = os.path.join(DOWNLOAD_DIR, "%(title)s.%(ext)s")
     ydl_opts = {
         "format": "bestaudio/best",
         "cookiefile": "cookies.txt",
         "outtmpl": output_path,
+        "extractor_args": {
+            "youtube": {
+                "player_client": ["android", "web", "ios"]
+            }
+        },
+        "http_headers": {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0 Safari/537.36"
+        },
         "postprocessors": [
             {
                 "key": "FFmpegExtractAudio",
@@ -32,23 +40,21 @@ def convert_to_wav(input_path: str) -> str:
     """Convert any audio/video file to WAV format using pydub."""
     output_path = os.path.splitext(input_path)[0] + "_converted.wav"
     audio = AudioSegment.from_file(input_path)
-    audio = audio.set_channels(1).set_frame_rate(16000) #16khz
+    audio = audio.set_channels(1).set_frame_rate(16000)  # 16khz
     audio.export(output_path, format="wav")
     return output_path
 # print(convert_to_wav(data))
 # wav_path=convert_to_wav(data)
 
-def chunk_audio(wav_path:str,chunk_minutes:int=10)->list:
-    audio=AudioSegment.from_wav(wav_path)
-    chunk_ms=chunk_minutes*60*1000
-    chunks=[]
-    for i ,start in enumerate(range(0,len(audio),chunk_ms)):
-        chunk=audio[start:start+chunk_ms]
-        chunk_path=f"{wav_path}_chunk_{i}.wav"
-        chunk.export(chunk_path,format="wav")
-        
+def chunk_audio(wav_path: str, chunk_minutes: int = 10) -> list:
+    audio = AudioSegment.from_wav(wav_path)
+    chunk_ms = chunk_minutes * 60 * 1000
+    chunks = []
+    for i, start in enumerate(range(0, len(audio), chunk_ms)):
+        chunk = audio[start:start + chunk_ms]
+        chunk_path = f"{wav_path}_chunk_{i}.wav"
+        chunk.export(chunk_path, format="wav")
         chunks.append(chunk_path)
-    
     return chunks
 # print(chunk_audio(wav_path))
 
@@ -64,4 +70,3 @@ def process_input(source: str) -> list:
     chunks = chunk_audio(wav_path)
     print(f"Audio ready — {len(chunks)} chunk(s) created.")
     return chunks
-
