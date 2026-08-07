0d952f5268949db0573f7c61bfb8a220cb2cf59f
venom312004 <dheerajpandey312004@gmail.com>
2026-08-07 21:34:29 +0530

    Use get_cookies_file for cloud cookies

--- utils/audio_processor.py
+++ utils/audio_processor.py
@@ -26,7 +26,7 @@ def download_youtube_audio(url: str) -> str:
     output_path = os.path.join(DOWNLOAD_DIR, "%(title)s.%(ext)s")
     ydl_opts = {
         "format": "bestaudio/best",
-        "cookiefile": "cookies.txt",
+        "cookiefile": get_cookies_file(),
         "outtmpl": output_path,
         "extractor_args": {
             "youtube": {
